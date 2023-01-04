d122cf7240b020dfb3d6fd14c4bb8035ee8689a3
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-01-04 16:13:29 +0400

    added struct annotations

--- version1/FileV1.go
+++ version1/FileV1.go
@@ -8,23 +8,23 @@ import (
 
 type FileV1 struct {
 	// Identification
-	Id    string
-	Group string
-	Name  string
+	Id    string `json:"id"`
+	Group string `json:"group"`
+	Name  string `json:"name"`
 
 	// Content
-	Description  string
-	ContentId    string
-	ContentUri   string
-	ThumbnailId  string
-	ThumbnailUri string
-	CreateTime   time.Time
-	ExpireTime   time.Time
-	Attributes   *data.StringValueMap
+	Description  string               `json:"description"`
+	ContentId    string               `json:"content_id"`
+	ContentUri   string               `json:"content_uri"`
+	ThumbnailId  string               `json:"thumbnail_id"`
+	ThumbnailUri string               `json:"thumbnail_uri"`
+	CreateTime   time.Time            `json:"create_time"`
+	ExpireTime   time.Time            `json:"expire_time"`
+	Attributes   *data.StringValueMap `json:"attributes"`
 
 	// Custom fields
-	CustomHdr any
-	CustomDat any
+	CustomHdr any `json:"custom_hdr"`
+	CustomDat any `json:"custom_dat"`
 }
 
 func NewFileV1(id, group, name, description, contentId, contentUri string, expireTime time.Time, attributes *data.StringValueMap) *FileV1 {
